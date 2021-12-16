d67da16bc593d3343552b7f5b80da6a656563b6c
zendepatil <sanketzende95@gmail.com>
2021-12-16 12:21:35 +0530

    compressed images

--- src/components/molecules/PopularDestination.js
+++ src/components/molecules/PopularDestination.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Row, Col } from "antd";
 import { Link } from "react-router-dom";
 import Recommendation from "../atoms/Recommendation/Recommendation";
-import { getRandomImage } from "../../utils/utils";
+import { imagArr } from "../../utils/utils";
 import WithLoading from "../../HOC/WithLoading";
 function PopularDestination({ isDestinationLoading }) {
   return (
@@ -10,29 +10,29 @@ function PopularDestination({ isDestinationLoading }) {
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
         <Col xs={24} sm={8} lg={10}>
           <Link to={`resortList?city=Maldives`}>
-            <Recommendation name={"Maldives"} background={getRandomImage()} />
+            <Recommendation name={"Maldives"} background={imagArr[0]} />
           </Link>
           <Link to={`resortList?city=Goa`}>
-            <Recommendation name={"Goa"} background={getRandomImage()} />
+            <Recommendation name={"Goa"} background={imagArr[1]} />
           </Link>
         </Col>
         <Col xs={24} sm={8} lg={6}>
           <Link to={`resortList?city=Carribean`}>
-            <Recommendation name={"Carribean"} background={getRandomImage()} />
+            <Recommendation name={"Carribean"} background={imagArr[6]} />
           </Link>
           <Link to={`resortList?city=Manali`}>
-            <Recommendation name={"Manali"} background={getRandomImage()} />
+            <Recommendation name={"Manali"} background={imagArr[7]} />
           </Link>
           <Link to={`resortList?city=Thailand`}>
-            <Recommendation name={"Thailand"} background={getRandomImage()} />
+            <Recommendation name={"Thailand"} background={imagArr[8]} />
           </Link>
         </Col>
         <Col xs={24} sm={8} lg={7}>
           <Link to={`resortList?city=Pondichery`}>
-            <Recommendation name={"Pondichery"} background={getRandomImage()} />
+            <Recommendation name={"Pondichery"} background={imagArr[13]} />
           </Link>
           <Link to={`resortList?city=Kerala`}>
-            <Recommendation name={"Kerala"} background={getRandomImage()} />
+            <Recommendation name={"Kerala"} background={imagArr[10]} />
           </Link>
         </Col>
       </Row>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,26 +1,19 @@
-import resort1 from "../assets/resort1.jpg";
-import resort2 from "../assets/resort2.jpg";
-import resort3 from "../assets/resort3.jpg";
-import resort4 from "../assets/resort4.jpg";
-import resort5 from "../assets/resort5.jpg";
-import resort6 from "../assets/resort6.jpg";
-import resort7 from "../assets/resort7.jpg";
-import resort8 from "../assets/resort8.jpg";
-import resort9 from "../assets/resort9.jpg";
-import resort10 from "../assets/resort10.jpg";
-import resort11 from "../assets/resort11.jpg";
-import resort12 from "../assets/resort12.jpg";
-import resort13 from "../assets/resort13.jpg";
-import resort15 from "../assets/resort15.jpg";
-import resort16 from "../assets/resort16.jpg";
-import resort18 from "../assets/resort18.jpg";
-import resort19 from "../assets/resort19.jpg";
-import resort20 from "../assets/resort20.jpg";
-import resort21 from "../assets/resort21.jpg";
-import resort22 from "../assets/resort22.jpg";
-import resort23 from "../assets/resort23.jpg";
-import resort24 from "../assets/resort24.jpg";
-import resort25 from "../assets/resort25.jpg";
+import resort2 from "../assets/resort-images/resort2.jpg";
+import resort5 from "../assets/resort-images/resort5.jpg";
+import resort6 from "../assets/resort-images/resort6.jpg";
+import resort7 from "../assets/resort-images/resort7.jpg";
+import resort9 from "../assets/resort-images/resort9.jpg";
+import resort10 from "../assets/resort-images/resort10.jpg";
+import resort11 from "../assets/resort-images/resort11.jpg";
+import resort12 from "../assets/resort-images/resort12.jpg";
+import resort13 from "../assets/resort-images/resort13.jpg";
+import resort15 from "../assets/resort-images/resort15.jpg";
+import resort19 from "../assets/resort-images/resort19.jpg";
+import resort20 from "../assets/resort-images/resort20.jpg";
+import resort21 from "../assets/resort-images/resort21.jpg";
+import resort22 from "../assets/resort-images/resort22.jpg";
+import resort24 from "../assets/resort-images/resort24.jpg";
+import resort25 from "../assets/resort-images/resort25.jpg";
 
 let debounceTimer;
 export const debounce = (cb, delay) => {
@@ -42,32 +35,25 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-const imagArr = [
-  resort1,
+export const imagArr = [
   resort2,
-  resort3,
-  resort4,
   resort5,
   resort6,
   resort7,
-  resort8,
   resort9,
   resort10,
   resort11,
   resort12,
   resort13,
   resort15,
-  resort16,
-  resort18,
   resort19,
   resort20,
   resort21,
   resort22,
-  resort23,
   resort24,
   resort25,
 ];
 
 export const getRandomImage = () => {
-  return imagArr[randomNumber(1, 25)];
+  return imagArr[randomNumber(0, 16)];
 };
