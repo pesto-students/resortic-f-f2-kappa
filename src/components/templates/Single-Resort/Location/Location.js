b75634695a4a7c71877d0bf298aa00cf61e2395e
sks-web <53891835+sks-web@users.noreply.github.com>
2021-12-17 12:43:09 +0530

    updated location

--- src/components/templates/Single-Resort/Location/Location.js
+++ src/components/templates/Single-Resort/Location/Location.js
@@ -1,18 +1,19 @@
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const Location = (props) => {
+  console.log(props);
   const center = {
-    lat: props.lat,
-    lng: props.lon,
+    lat: Number(props.lat),
+    lng: Number(props.lon),
   };
   const containerStyle = {
     width: "100%",
     height: "400px",
   };
   return (
-    <div style={{ marginTop: "20px", marginBottom: "20px" }}>
-      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
-        <GoogleMap mapContainerStyle={containerStyle} zoom={10}>
+    <div>
+      <LoadScript googleMapsApiKey="AIzaSyAqMRNsVtp3Nq05lLynv4T9C9JCwVqyq6Q">
+        <GoogleMap center={center} mapContainerStyle={containerStyle} zoom={10}>
           {/* Child components, such as markers, info windows, etc. */}
           <Marker position={center} />
         </GoogleMap>
